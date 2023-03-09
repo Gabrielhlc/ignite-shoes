b3869b6c60a9c2cf644cc75a7a6565b6d8405e05
Gabrielhlc <gabrielhenlc@gmail.com>
2023-03-09 16:46:04 -0300

    add: Push Notifications

--- App.tsx
+++ App.tsx
@@ -1,17 +1,46 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import OneSignal from 'react-native-onesignal';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { useEffect } from 'react';
+
+
+OneSignal.setAppId('53a07757-9d93-423c-ab3a-6032c00d6a01');
+
+OneSignal.setEmail('gabriel@email.com');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  tagUserInfoCreate();
+
+
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) => {
+
+      const { actionId } = response.action as any;
+
+      switch (actionId) {
+        case '1':
+          return console.log('Ver todas');
+        case '2':
+          return console.log('Ver pedido');
+        default:
+          return console.log('Não foi clicado em botão de ação');
+      }
+
+    })
+
+    return () => unsubscribe;
+  }, [])
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
@@ -22,6 +51,7 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
     </NativeBaseProvider>
   );
 }
